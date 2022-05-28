89deaf6a595dccbd6d1dd5f6eea8ac61c53503c0
res876ttb <someone10851222@gmail.com>
2022-05-28 15:35:55 +0800

    Add profile

---
+++ Profile/precache-manifest.12f52e061a8f76578f5a2a807d40238f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b41bbbc0ab4b8c964fa595ac2e57de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "781a773dae7f61a1e5ad",
+    "url": "/static/css/main.f74c9d1c.chunk.css"
+  },
+  {
+    "revision": "985d05ee02115024f10b",
+    "url": "/static/js/2.7330edfa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7330edfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781a773dae7f61a1e5ad",
+    "url": "/static/js/main.ba1a7d8b.chunk.js"
+  },
+  {
+    "revision": "343aae8aebdaf4fdab6d",
+    "url": "/static/js/runtime-main.4cfff493.js"
+  }
+]);
